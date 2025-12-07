6773108678371acee7fcd0d45448d5337ac931f6
Pritiranjan Mohanty <pritiranjan.mohanty2003@gmail.com>
2025-12-07 10:21:24 +0530

    add vercel config

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 window.APP_CONFIG = {
-  // API_BASE_URL: "http://localhost:8080"
-  API_BASE_URL: "http://springbootrestapi-env.eba-bugmbvqw.ap-south-1.elasticbeanstalk.com",
+  API_BASE_URL: "/api"
 };
